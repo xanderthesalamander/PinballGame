835a9f034024e599ffd09e5879e47fd244a854c4
Adam Walton <adamrwalton@gmail.com>
2023-08-08 07:32:26 +0300

    GDV-34 Create Scoreboard and add relevant scripts

---
+++ Assets/Scripts/ScoreableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreableObject : MonoBehaviour
+{
+    // This script is added to any oject that can score points
+    public int scoreValue;
+    private int defaultScoreValue = 25;
+
+    void Start ()
+    {
+        if(scoreValue == 0){
+            scoreValue = defaultScoreValue;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+         // If collision with a ball object
+        if (collision.gameObject.tag == "ball")
+        {
+            GameManager.Instance.Score += scoreValue;
+        }
+    }
+}
