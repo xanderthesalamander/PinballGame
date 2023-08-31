2ec493da88061ea82c261085090a1a03f7ba6d76
Adam Walton <adamrwalton@gmail.com>
2023-08-31 19:26:08 +0300

    GDV-84 Create simple state machine to handle quests

---
+++ Assets/Scripts/MissionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    public Animator missionStateAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        missionStateAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(missionStateAnimator.GetCurrentAnimatorStateInfo(0).IsName("Between Missions")){
+            Debug.Log("Between Missions");
+        }
+        //missionStateAnimator.Set[Bool]("MissionComplete", true)
+    }
+}
