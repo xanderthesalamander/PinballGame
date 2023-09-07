deebfc0f58f9ca6552e0f392259f21de33938ec0
Steven Chiu <schiu01@gmail.com>
2023-09-07 16:33:45 -0400

    GDV-94

---
+++ Assets/Scripts/SpotLightOnBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightOnBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 offset;
+    
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject ball = GameObject.FindGameObjectWithTag("ball");
+        
+
+        transform.position = ball.transform.position + offset;
+    }
+}
