8d39cc7926bb87a55ba2f84b3d6def6895b9fae1
Shuvojit18 <shuvojit18chakraborty@gmail.com>
2023-09-09 21:55:39 +0530

    Background music and game feel
    - new main scene Main12
    - adds a responsive background music "let go"
    - as players lives decrease, music gets faster creating excitement and fun

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour
     public int Score;
     public short Lives;
     ParticleSystem Confetti;
+    AudioSource BGM;
 
     private void Awake()
     {
@@ -23,20 +24,19 @@ public class GameManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
-        
+        Confetti = GameObject.Find("Confetti").GetComponent<ParticleSystem>();
+        BGM = GameObject.Find("BGM").GetComponent<AudioSource>();
+        BGM.Play();
     }
 
     public void Start()
     {
         Score = 0;
         Lives = 3;
-        Confetti = GameObject.Find("Confetti").GetComponent<ParticleSystem>();
     }
 
     public void Update()
     {
-        
-
         
     }
 
@@ -44,7 +44,14 @@ public class GameManager : MonoBehaviour
         if(Score > HighScore){
             HighScore = Score;
         }
-
+        if(Lives == 3){
+            BGM.pitch = 2;
+        } else if (Lives == 2){
+            BGM.pitch = 3;
+        } else {
+            BGM.pitch = 1;
+        }
+        
     }
     
 
@@ -54,6 +61,5 @@ public class GameManager : MonoBehaviour
             emission.rateOverTime = HighScore;
             Confetti.Play();
         }
-        
     }
 }
